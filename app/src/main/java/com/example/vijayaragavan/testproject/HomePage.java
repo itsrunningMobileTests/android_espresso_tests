ca4d70e921cd3d8e59f51ec173e2002ba1db97b7
vijayaragavan <vijayaragavan.g@gmail.com>
2017-03-13 12:11:47 +0530

    first commit

---
+++ app/src/main/java/com/example/vijayaragavan/testproject/HomePage.java
@@ -0,0 +1,94 @@
+package com.example.vijayaragavan.testproject;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HomePage extends AppCompatActivity implements OnClickListener {
+
+
+    EditText etNum1;
+    EditText etNum2;
+    TextView etResult;
+
+    Button btnAdd;
+    Button btnSub;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+//         find the elements
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
+        etResult =(TextView) findViewById(R.id.tvResult);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+
+
+        btnAdd.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+
+        // check if the fields are empty
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+        }
+
+        // read EditText and fill variables with numbers
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
+
+        // defines the button that has been clicked and performs the corresponding operation
+        // write operation into oper, we will use it later for output
+        switch (v.getId()) {
+            case R.id.btnAdd:
+
+                result = num1 + num2;
+                break;
+            default:
+                break;
+        }
+
+
+//        etResult.setText( "vijay");
+        // form the output line
+        etResult.setText( Float.toString(result));
+
+
+    }
+}
